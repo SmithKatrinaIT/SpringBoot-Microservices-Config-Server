90b17f30435a3525400bb35d307a8bad820d6fa7
SmithKatrinaIT <smith.katrina.it@gmail.com>
2023-04-02 11:54:38 -0400

    update

--- src/main/java/com/smithexperience/configserver/ConfigServerApplication.java
+++ src/main/java/com/smithexperience/configserver/ConfigServerApplication.java
@@ -2,12 +2,29 @@ package com.smithexperience.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * as of Spring Boot 3 and Spring Cloud 2022.0.0 no longer need to "enable" the Eureka client with the @EnableEurekaClient
+ * annotation at the APP level (main class) -- it is auto-configured when adding the Eureka dependency
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    /**
+     * Spring Cloud Config Server: a Spring Boot application with the Spring Cloud Config Server library dependency
+     *  -- Spring Cloud Config server allows for the ability to 'pull-in' all configuration files from a centralized location
+     *     that pertain to/are used by the [entire] application.
+     *     -- Config changes are done in ONE place -- keeping the code 'clean and manageable'
+     *  -- The config files contain all the 'properties' of the microservices [config client] registered to/with the config server
+     *     -- The 'config client' [microservice] is created/established by defining the config server dependency [location/url] within the
+     *        properties file of the service
+     *     -- When the services start, the [micro] service will load all the configuration files from the config server
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 
 }
